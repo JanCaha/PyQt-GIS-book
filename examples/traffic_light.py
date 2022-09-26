46ca1d688f5d9606f4847f1acfbeeb25cf5e1c97
Jan Caha <jan.caha@outlook.com>
2022-09-26 13:31:33 +0200

    update widget functionality

--- examples/traffic_light.py
+++ examples/traffic_light.py
@@ -9,21 +9,8 @@ from PyQt5.QtGui import QPainter, QBrush, QColor, QPaintEvent, QPen, QMouseEvent
 
 class LightStatus(Enum):
     RED = 1
-    ORANGE1 = 2
+    ORANGE = 2
     GREEN = 3
-    ORANGE2 = 4
-
-    def next(self):
-        v = self.value
-        if v == 4:
-            return LightStatus.RED
-        return LightStatus(v + 1)
-
-    def prev(self):
-        v = self.value
-        if v == 1:
-            return LightStatus.ORANGE2
-        return LightStatus(v - 1)
 
 
 class TrafficLight(QWidget):
@@ -35,18 +22,43 @@ class TrafficLight(QWidget):
                  flags: Union[Qt.WindowFlags, Qt.WindowType] = Qt.WindowType.Widget) -> None:
         super().__init__(parent, flags)
         self._light_status = light_status
+        self._previous_light_status = None
         self._read_only = read_only
         self._color_red = QColor('#D60000')
         self._color_orange = QColor('#D6B100')
         self._color_green = QColor('#00AE23')
+        self._color_background = QColor('#000000')
         self._darken_by = 300
         self._circle_radius: float = None
         self._vertical_offset: float = None
 
+    def set_color(self, light_status: LightStatus, color: QColor) -> None:
+        if light_status == LightStatus.RED:
+            self._color_red = color
+        if light_status == LightStatus.GREEN:
+            self._color_green = color
+        if light_status == LightStatus.ORANGE:
+            self._color_orange = color
+
+    def color(self, light_status: LightStatus) -> QColor:
+        if light_status == LightStatus.RED:
+            return self._color_red
+        if light_status == LightStatus.GREEN:
+            return self._color_green
+        if light_status == LightStatus.ORANGE:
+            return self._color_orange
+
+    def background_color(self) -> QColor:
+        return self._color_background
+    
+    def set_background_color(self, color: QColor) -> None:
+        self._color_background = color
+    
     def circle_radius(self) -> int:
         return min(self.height() * 0.3, self.width() * 0.9)
 
-    def drawCircle(self, painter: QPainter, center: QPointF, radius: float, color: QColor) -> None:
+    def draw_circle(self, painter: QPainter, center: QPointF, radius: float,
+                    color: QColor) -> None:
         painter.save()
 
         painter.setRenderHint(QPainter.Antialiasing)
@@ -64,32 +76,32 @@ class TrafficLight(QWidget):
         painter.drawEllipse(center, radius, radius)
         painter.restore()
 
-    def color_status(self, color: QColor, turned_off_status: bool) -> QColor:
+    def color_based_on_status(self, color: QColor, turned_off_status: bool) -> QColor:
         if turned_off_status:
             color = color.darker(self._darken_by)
         return color
 
     @property
     def red_color(self) -> QColor:
-        return self.color_status(self._color_red, self._light_status != LightStatus.RED)
+        return self.color_based_on_status(self._color_red, self._light_status != LightStatus.RED)
 
     @property
     def orange_color(self) -> QColor:
-        return self.color_status(
-            self._color_orange, self._light_status
-            not in [LightStatus.ORANGE1, LightStatus.ORANGE2])
+        return self.color_based_on_status(
+            self._color_orange, self._light_status != LightStatus.ORANGE)
 
     @property
     def green_color(self) -> QColor:
-        return self.color_status(self._color_green, self._light_status != LightStatus.GREEN)
+        return self.color_based_on_status(self._color_green,
+                                          self._light_status != LightStatus.GREEN)
 
-    def checkedState(self) -> Qt.CheckState:
-        if self._light_status == LightStatus.GREEN:
-            return Qt.Checked
-        if self._light_status == LightStatus.RED:
-            return Qt.Unchecked
-        if self._light_status in [LightStatus.ORANGE1, LightStatus.ORANGE2]:
-            return Qt.PartiallyChecked
+    # def checkedState(self) -> Qt.CheckState:
+    #     if self._light_status == LightStatus.GREEN:
+    #         return Qt.Checked
+    #     if self._light_status == LightStatus.RED:
+    #         return Qt.Unchecked
+    #     if self._light_status in [LightStatus.ORANGE1, LightStatus.ORANGE2]:
+    #         return Qt.PartiallyChecked
 
     def setReadOnly(self, readOnly: bool) -> None:
         self._read_only = readOnly
@@ -105,7 +117,7 @@ class TrafficLight(QWidget):
     def main_shape(self) -> QRect:
         return self._rectangle(self._circle_radius, self._vertical_offset)
 
-    def calculate_sizes(self) -> None:
+    def _calculate_sizes(self) -> None:
         if self.height() * 0.3 < self.width() * 0.9:
             self._circle_radius = (self.height() * 0.3) / 2
             self._vertical_offset = (self.height() * 0.1) / 4
@@ -113,40 +125,70 @@ class TrafficLight(QWidget):
             self._circle_radius = (self.width() * 0.9) / 2
             self._vertical_offset = (self._circle_radius / 3) / 4
 
-    def paintEvent(self, e: QPaintEvent):
+    def draw_rectangle(self, painter: QPainter) -> None:
 
-        painter = QPainter(self)
-
-        self.calculate_sizes()
+        self._calculate_sizes()
 
         painter.save()
 
         brush = QBrush()
-        brush.setColor(QColor('black'))
+        brush.setColor(self._color_background)
         brush.setStyle(Qt.SolidPattern)
 
         painter.fillRect(self.main_shape(), brush)
 
         painter.restore()
 
+    def paintEvent(self, e: QPaintEvent):
+
+        painter = QPainter(self)
+
+        self.draw_rectangle(painter)
+
         width_midpoint = self.width() / 2
 
-        self.drawCircle(painter,
-                        QPointF(width_midpoint, self._vertical_offset + self._circle_radius),
-                        self._circle_radius, self.red_color)
+        self.draw_circle(painter,
+                         QPointF(width_midpoint, self._vertical_offset + self._circle_radius),
+                         self._circle_radius, self.red_color)
 
-        self.drawCircle(
+        self.draw_circle(
             painter,
             QPointF(width_midpoint,
                     self._vertical_offset * 2 + self._circle_radius + self._circle_radius * 2),
             self._circle_radius, self.orange_color)
 
-        self.drawCircle(
+        self.draw_circle(
             painter,
             QPointF(width_midpoint,
                     self._vertical_offset * 3 + self._circle_radius * 4 + self._circle_radius),
             self._circle_radius, self.green_color)
 
+    def select_next_status(self) -> None:
+        if self._light_status in [LightStatus.GREEN, LightStatus.RED]:
+            self._previous_light_status = self._light_status
+            self._light_status = LightStatus.ORANGE
+        else:
+            if self._previous_light_status == LightStatus.GREEN:
+                self._previous_light_status = self._light_status
+                self._light_status = LightStatus.RED
+            if self._previous_light_status == LightStatus.RED:
+                self._previous_light_status = self._light_status
+                self._light_status = LightStatus.GREEN
+    
+    def select_previous_status(self) -> None:
+        if self._light_status in [LightStatus.GREEN, LightStatus.RED]:
+            self._previous_light_status = self._light_status
+            self._light_status = LightStatus.ORANGE
+        else:
+            if self._previous_light_status == LightStatus.GREEN:
+                tmp = self._previous_light_status 
+                self._previous_light_status = self._light_status
+                self._light_status = tmp
+            if self._previous_light_status == LightStatus.RED:
+                tmp = self._previous_light_status
+                self._previous_light_status = self._light_status
+                self._light_status = tmp
+        
     def event(self, event: QEvent) -> bool:
 
         if event.type() == QEvent.ToolTip:
@@ -156,7 +198,7 @@ class TrafficLight(QWidget):
                     tooltip = "Red"
                 if self._light_status == LightStatus.GREEN:
                     tooltip = "Green"
-                if self._light_status in [LightStatus.ORANGE1, LightStatus.ORANGE2]:
+                if self._light_status == LightStatus.ORANGE:
                     tooltip = "Orange"
                 QToolTip.showText(event.globalPos(), tooltip)
             else:
@@ -170,9 +212,9 @@ class TrafficLight(QWidget):
     def mouseReleaseEvent(self, a0: QMouseEvent) -> None:
         if not self._read_only:
             if a0.button() == Qt.LeftButton:
-                self._light_status = self._light_status.next()
+                self.select_next_status()
             if a0.button() == Qt.RightButton:
-                self._light_status = self._light_status.prev()
+                self.select_previous_status()
             self.update()
         return super().mouseReleaseEvent(a0)
 
